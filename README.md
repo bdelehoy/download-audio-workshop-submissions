6fabd801251b9afb167f2cf5866039972f483533
Brandon Delehoy <bdelehoy@yahoo.com>
2019-04-24 17:59:02 -0700

    Add 'maybe' and custom download folder support + more documentation

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
-This Python 3 script uses a CSV file of Google Form responses, converts all youtube links inside to MP3 files, and organizes them appropriately in your Downloads folder.
+This Python 3 script accepts a CSV file of Google Form responses, converts all youtube links inside to MP3 files, and organizes them in a folder of your choice (defaults to your Downloads folder). Download it with the big green "Clone or Download" button up there^ somewhere and download the ZIP.
 
-Windows only.  Some modification may be required for use with other platforms.
+I've confirmed that this script works with Windows 10.  Some modification *may* be required for use with other platforms, although youtube-dl does a good job of santizing file paths.
 
+Run it like this in a command prompt or terminal:
 
+`python read_audioworkshop_form.py C:/path/to/your/form.csv C:/path/to/your/folder/of/choice`
 
-Requires the programs 'youtube-dl' and 'ffmpeg' in your PATH.  Get them here:
+Requires the programs `youtube-dl` and `ffmpeg` in your PATH variable. And Python 3. Get them here:
 
 * [youtube-dl](https://rg3.github.io/youtube-dl/)
+    * [youtube-dl downloads page](https://ytdl-org.github.io/youtube-dl/download.html) - you need the Windows .exe and also the C++ package that they mention here. Install that package and youtube-dl should just  work in a command prompt.
 * [ffmpeg](https://www.ffmpeg.org/download.html)
-    * [ffmpeg Windows build](https://ffmpeg.zeranoe.com/builds/)
+    * [ffmpeg Windows build](https://ffmpeg.zeranoe.com/builds/) - this should be what you need if you want to run this Python script on a Windows machine.
+
+(I'd recommend making a folder in Program Files so you can put these where they seem fit, but you do you. Just don't put them anywhere where they could be deleted.)
+
+* [and Python 3.](https://www.python.org/)
+
+How to edit your PATH variable on Windows 10:
+
+1. Open the Control Panel (just type "Control Panel" on the Start menu)
+2. Click "System"
+3. Click "Advanced System Settings" on the left panel
+4. Click "Environment Variables"
+5. In the upper list, find "Path". Click on it and then click the "Edit" button just below the list. This should open up a window that has a neat list of every folder currently in your PATH right now - it could be empty, it could not.
+6. Open a file explorer and navigate to the folder you want to add. Click on the file address bar and copy it.
+7. In the Path editing window that opened in step 5, click "New" on the top-right and paste in the address from step 6.
+8. Click "OK" and close all the windows that popped up. You're done!
+
+Run these steps for the folders that contain the files `youtube-dl.exe` and `ffmpeg.exe`. You should find the ffmpeg .exe's in the `/bin` folder. `/bin` is short for "binary" and this is where you can find executable programs in lots of applications. These programs can now be called from anywhere you are on your computer, which this Python script relies on.
+
+* If typing `python` in the command prompt does nothing, then a possible error could be that Python isn't on your PATH. If you're installing Python for the first time, there should be an option somewhere to add Python to your PATH variable. Make sure that box is ticked! If you have Python installed already but it's not on your PATH, you can always add it yourself with the steps above. If you're on Windows, I think its default folder should be somewhere in `C:\Program Files\`, or maybe `C:\Users\YOUR_USERNAME\AppData\Local\Programs\Python\` - you're looking for the folder that has `python.exe`.
+
+* If there's further trouble with `youtube-dl` throwing up "ffprobe or avprobe not found", here's [a helpful thread on stackoverflow](https://stackoverflow.com/a/38878753).
+    * Most youtube-dl problems should be solvable by updating it in an elevated/admin terminal: `youtube-dl -U`
--- read_audioworkshop_form.py
+++ read_audioworkshop_form.py
@@ -1,47 +1,100 @@
-from csv import reader
-import subprocess
+from csv import reader  # To parse the CSV file that you download from Google Forms
+from sys import argv    # To read the command-line arguments
+import subprocess       # To call youtube-dl (see the readme file)
 
 ### Originally written by Brandon Delehoy
 ### for the Audio Department of the Video Game Development Club at UC Irvine
 ### twitter: @_brandolf
 
-# CSV format for each row:
+### last update: 2019 April 24
+
+# CSV format for each row (if you modify the survey in the future, make sure to see if this changes!):
 # row[0]: timestamp (not really needed)
 # row[1]: submitter's name
-# row[2]: 'Yes'/'No' (attending the workshop?)
+# row[2]: 'Yes'/'No'/'Maybe' (are they attending the workshop?)
 # row[3]: ***THE LINK***
 
-# change this to whatever path the csv is located in
-my_csv_path = './example_form.csv'
+###########
+### STARTUP
+# the folder that all the files should be downloaded to - by default:
+#             "~/Downloads/"    (the '~' means "home" and is standard on Linux... and Windows too, apparently!)
+# if this default throws up errors, just use this instead:
+#             "C:/Users/YOUR_USER_NAME_HERE/Downloads/"
+# include the "/" at the end!!!
+DOWNLOAD_TO = "~/Downloads/"
 
-print("Please open this python script and edit my_csv_path with the path of your survey results in CSV form.")
+if len(argv) == 1:
+    # if no arguments were provided, display a help string
+    print("\n************************************************************************************")
+    print("*** ~ Bulk YouTube to MP3 Downloading Script For Audio Department Survey Forms ~ ***")
+    print("************************************************************************************")
+    
+    print("\nHOW TO USE:")
+    
+    print("\nSupply a valid path to a CSV file as a command-line parameter, and optionally a folder to download everything to (include a '/' at the end!!).")
+    print("Example:\n\t\tpython read_audioworkshop_form.py ./example_form.csv C:/Users/Brandon/Downloads/Workshop04242019")
+    
+    print("\nIf you're going to change the format of the survey in the future, feel free to modify this script as needed. I've tried to document it well, but if anything's unclear please let me know!")
+    
+    print("\nand please read the readme if any errors pop up ty")
+    exit(0)
+else:
+    MY_CSV_PATH = argv[1]
+    print("Will attempt to download links from:\t"+MY_CSV_PATH)
+    # check if the file provided ends in ".csv"
+    if len(MY_CSV_PATH) >= 4 and MY_CSV_PATH[-4:].lower() != ".csv":
+        raise Exception("Please provide a path to a CSV file. You wrote:\n>>\t" + MY_CSV_PATH)
+    # now check if any specific download folders were provided and sanitize:
+    if len(argv) >= 3:
+        DOWNLOAD_TO = argv[2]
+        # this is where cross-platform file paths would have to be specified - also later on in path_to_download_to.
+        if DOWNLOAD_TO[-1] not in "\\/":
+            raise Exception("Please add your operating system's file path separator to the end of your specified download folder:\n\tWindows: \t\\\n\tLinux/OS X: \t/\nYou wrote:\n>>\t" + DOWNLOAD_TO)
 
-with open(my_csv_path) as f:
+print("Will download files to:\t\t\t"+DOWNLOAD_TO)
+
+###################
+### THE DOWNLOADIN'
+# file is automatically closed after because "with" is used
+with open(MY_CSV_PATH) as f:
     my_reader = reader(f)
-    print("CSV file successfully read: {}".format(my_csv_path))
-    #print("CSV file successfully read: {}\nFound {} videos to download".format(my_csv_path, sum(1 for row in my_reader)-1))
+    print("CSV file successfully read.")
+    #print("CSV file successfully read: {}\nFound {} videos to download".format(MY_CSV_PATH, sum(1 for row in my_reader)-1))
     # wanted to print out amount of entries, but... generators.... (cannot read anymore since we exhausted it)
-    input("Press Enter to continue...")
+    print("Remember to update youtube-dl if any errors come up! Run the following command:\n\tyoutube-dl -U\nin an admin command prompt or terminal.")
+    input("\nPress Enter to continue...\n")
     count = 1
     for row in my_reader:
         # skip the first row (it's a header)
         if row[3] != 'Streaming link to your song choice:':
-            # if the person is attending, place them higher in the file order (so their submission shows up first)
+            # if the person is attending or could be attending, place them higher in the file order (so their submission shows up first)
+            persons_name = row[1].replace(" ", "_") # replace the spaces in their name to underscores; for use in the file that we download to
+            attending = 0
             if row[2] == "Yes":
-                # add an "_" at the beginning to their file to put them at the top
-                debug_print = "{} (who is attending) submitted: {}\n   Downloading now....".format(row[1], row[3])
-                path_to_download_to = "~/Downloads/_{}_{}_%(title)s_%(id)s.%(ext)s".format(count, row[1].replace(" ", "_"))
+                # add an "__" at the beginning to their file to put them at the top
+                path_to_download_to = DOWNLOAD_TO+"__{}_{}___%(title)s_%(id)s.%(ext)s".format(count, persons_name)
+            elif row[2] == "Maybe":
+                # add "_" at the beginning to their file to put them in a second, middle category
+                path_to_download_to = DOWNLOAD_TO+"_{}_{}___%(title)s_%(id)s.%(ext)s".format(count, persons_name)
             else:
                 # there's no "_" at the beginning, so their file will be at the bottom
-                debug_print = "{} (NOT attending) submitted: {}\n   Downloading now....".format(row[1], row[3])
-                path_to_download_to = "~/Downloads/{}_{}_%(title)s_%(id)s.%(ext)s".format(count, row[1].replace(" ", "_"))
+                path_to_download_to = DOWNLOAD_TO+"{}_{}___%(title)s_%(id)s.%(ext)s".format(count, persons_name)
             
-            youtube_dl_string = 'youtube-dl -x --audio-format mp3 -o {} --no-check-certificate --no-playlist '.format(path_to_download_to)
-            print ("\n{}\n".format(count))
-            print(debug_print)
-            print("EXECUTE:", youtube_dl_string)
-            subprocess.call(youtube_dl_string+row[3])
+            print("Submission #{}:".format(count))
+            print("\tName:\t\t" + row[1])
+            print("\tAttending?\t" + row[2].upper())
+            print("\tSubmitted at:\t" + row[0])
+            print("\tLink:\t\t" + row[3])
+            print("\tSaving to:\t" + path_to_download_to)
+            
+            # the string that we use to call youtube-dl, with all of its own arguments and flags
+            # pro-tip: if you want to keep all the videos, add "-k" (without quotes) to this string, at the very end (after the second set of braces)
+            youtube_dl_string = 'youtube-dl -x --audio-format mp3 -o {} --no-check-certificate --no-playlist {}'.format(path_to_download_to, row[3].strip())
+            print("EXECUTE:\n"+youtube_dl_string)
+            subprocess.call(youtube_dl_string)   # call youtube-dl! this could take a while.
+            
+            print ("Submission {} complete.\n".format(count))
             count += 1
-    print("\n\nDone!")
+    print("\nDone!")
     print("If youtube-dl spat out any errors, try updating it with\n   youtube-dl -U")
     print("(you might need to run cmd or Powershell in Administrator Mode to do that)")
